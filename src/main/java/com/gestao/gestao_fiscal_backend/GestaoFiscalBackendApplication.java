03ca69d9742e57fbdf92fd91e2fe326360dfe7ea
Marcos <marcos.fernandes@mupisystems.com.br>
2025-09-04 20:05:48 -0300

    :tada: init: primeiro commit

---
+++ src/main/java/com/gestao/gestao_fiscal_backend/GestaoFiscalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gestao.gestao_fiscal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoFiscalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoFiscalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestao/gestao_fiscal_backend/GestaoFiscalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestao.gestao_fiscal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFiscalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

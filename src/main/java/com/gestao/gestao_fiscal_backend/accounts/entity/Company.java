20dbb3e20b4fe450f9940a6023b419e1c857d850
MuriloSG <muriloprojetosti@gmail.com>
2025-09-08 22:07:37 -0300

    feat: adicionar metodo para retornar os employees da company

--- src/main/java/com/gestao/gestao_fiscal_backend/accounts/entity/Company.java
+++ src/main/java/com/gestao/gestao_fiscal_backend/accounts/entity/Company.java
@@ -2,6 +2,8 @@ package com.gestao.gestao_fiscal_backend.accounts.entity;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name="companies")
 public class Company {
@@ -15,5 +17,12 @@ public class Company {
 
   @Column(name="isActive")
   private Boolean isActive;
+
+  @OneToMany(mappedBy = "company", fetch = FetchType.LAZY)
+  private List<Employee> employees;
+
+  public List<Employee> getEmployees() {
+    return employees;
+  }
 
 }
